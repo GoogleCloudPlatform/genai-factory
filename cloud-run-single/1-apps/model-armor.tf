287044740f7306d5978d981c6c3e7e5c3a89fdca
Aurélien Legrand <aurelien.legrand01@gmail.com>
2025-07-25 10:12:00 +0200

    Creating Model Armor template

---
+++ cloud-run-single/1-apps/model-armor.tf
@@ -0,0 +1,52 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_model_armor_template" "model-armor-template" {
+  project = var.project_config.id
+  location    = var.region
+  template_id = "model-armor-template"
+  filter_config {
+    rai_settings {
+      rai_filters {
+        filter_type      = "HATE_SPEECH"
+        confidence_level = "MEDIUM_AND_ABOVE"
+      }
+    }
+    sdp_settings {
+      basic_config {
+        filter_enforcement = "ENABLED"
+      }
+    }
+    pi_and_jailbreak_filter_settings {
+      filter_enforcement = "ENABLED"
+      confidence_level   = "HIGH"
+    }
+    malicious_uri_filter_settings {
+      filter_enforcement = "ENABLED"
+    }
+  }
+  template_metadata {
+    custom_llm_response_safety_error_message = "This is a custom error message for LLM response"
+    log_template_operations                  = true
+    log_sanitize_operations                  = true
+    multi_language_detection {
+      enable_multi_language_detection = true
+    }
+    ignore_partial_invocation_failures    = true
+    custom_prompt_safety_error_code       = 400
+    custom_prompt_safety_error_message    = "This is a custom error message for prompt"
+    custom_llm_response_safety_error_code = 401
+    enforcement_type                      = "INSPECT_ONLY"
+  }
+}
