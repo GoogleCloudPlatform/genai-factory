8bdebdb91e1d677c4fda4a5eb703f9512236fe4a
Luca Prete <preteluca@gmail.com>
2025-11-06 16:49:23 +0100

    Upgrade to Fabric v46.1.0

--- cloud-run-rag/0-projects/main.tf
+++ cloud-run-rag/0-projects/main.tf
@@ -20,7 +20,7 @@ locals {
 }
 
 module "projects" {
-  source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/project-factory?ref=v46.0.0"
+  source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/project-factory?ref=v46.1.0"
   data_defaults = {
     billing_account = var.project_config.billing_account_id
     parent          = var.project_config.parent
