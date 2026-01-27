075ec5df1a79efd07589833a6aa553f57cd368c7
Luca Prete <preteluca@gmail.com>
2026-01-27 13:52:35 +0300

    [fix] Set max/min instances in cloud-run-* modules using the correct variables. (#134)

--- cloud-run-rag-search/1-apps/ingestion.tf
+++ cloud-run-rag-search/1-apps/ingestion.tf
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,7 +42,10 @@ module "cloud_run_ingestion" {
   service_config = {
     gen2_execution_environment = true
     ingress                    = var.cloud_run_configs.ingestion.ingress
-    max_instance_count         = var.cloud_run_configs.ingestion.max_instance_count
+    scaling = {
+      max_instance_count = var.cloud_run_configs.ingestion.max_instance_count
+      min_instance_count = var.cloud_run_configs.ingestion.min_instance_count
+    }
   }
 }
 
