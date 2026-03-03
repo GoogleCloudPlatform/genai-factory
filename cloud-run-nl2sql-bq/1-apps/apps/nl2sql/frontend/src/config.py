2ba21364c1f5b4b1eace8dbe2f1cb7e232e51194
Thomas Zettl <zettlt@google.com>
2026-03-03 17:41:19 +0000

    Initial nl2sql commit

---
+++ cloud-run-nl2sql-bq/1-apps/apps/nl2sql/frontend/src/config.py
@@ -0,0 +1,34 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+PROJECT_ID = os.environ.get("PROJECT_ID")
+
+REGION = os.environ.get("REGION", "europe-west1")
+
+ROOT_AGENT_MODEL = os.environ.get("MODEL_NAME", "gemini-2.5-flash")
+BQ_COMPUTE_PROJECT_ID = os.environ.get("MODEL_NAME", PROJECT_ID)
+BQ_DATA_PROJECT_ID = os.environ.get("MODEL_NAME", PROJECT_ID)
+BQ_DATASET_ID = os.environ.get("BQ_DATASET_ID")
+BQ_TABLE_NAME = os.environ.get("BQ_TABLE_NAME")
+
+INTERNAL_PORT = int(os.environ.get("PORT", 8080))
+
+AGENT_DIR = os.environ.get(
+    "AGENT_DIR", os.path.join(os.path.dirname(os.path.abspath(__file__)), "agents")
+)
+
+ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS", ["*"])
+SERVE_WEB_INTERFACE = os.environ.get("SERVE_WEB_INTERFACE", False)
