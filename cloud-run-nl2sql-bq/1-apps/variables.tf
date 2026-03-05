0f509eaa6ca500883c80ff533d10c79fdb869965
Thomas Zettl <zettlt@google.com>
2026-03-05 19:55:15 +0000

    Remove unnecessary tf var

--- cloud-run-nl2sql-bq/1-apps/variables.tf
+++ cloud-run-nl2sql-bq/1-apps/variables.tf
@@ -12,13 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "alloydb_initial_password" {
-  description = "Initial password for the postgres user in AlloyDB."
-  type        = string
-  nullable    = false
-  default     = "CHANGE_THIS!!!"
-}
-
 # Only used when ILBs are created.
 # CA pools can't be recreated in the same project with the same name.
 # This can be handy during experimentation
