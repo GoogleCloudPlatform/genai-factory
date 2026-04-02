a4cc2288a72a287873c96bcc1d150367014d9313
Luca Prete <preteluca@gmail.com>
2026-04-02 15:26:20 +0200

    Point to Cloud Foundation Fabric v54.2.0 (#185)

--- agent-engine/1-apps/main.tf
+++ agent-engine/1-apps/main.tf
@@ -19,7 +19,7 @@ data "archive_file" "source" {
 }
 
 module "agent" {
-  source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/agent-engine?ref=v54.1.0"
+  source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/agent-engine?ref=v54.2.0"
   name       = var.name
   project_id = var.project_config.id
   region     = var.region
